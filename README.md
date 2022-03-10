04cb676be54277b9f089aaa5fe9e0de412a6d820
Alexander Flanagan <alex.flanagan@gmail.com>
2022-03-10 17:44:14 -0500

    Add output and examples.

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A tool for generating a base64-encoded protocol-buffer-packed public key from an IPFS secret key.
 
+## Example Output
+
+```bash
+SECRET KEY (NOT FOR SHARING)	 CAESQHk2ZJ7Wte0QPkb+6INVRwZpo2FCeYbfnvfzHvMU/kwiX4z6CISXQFKp609TikAk1i/gEhTbMuD/lH9VRX2NMvY=
+PUBLIC KEY (SHARE WITH KUBELT)	 CAESIF+M+giEl0BSqetPU4pAJNYv4BIU2zLg/5R/VUV9jTL2
+```
+
 ## Prerequisites
 
 Requires an IPFS node running locally.
--- keytool.js
+++ keytool.js
@@ -13,11 +13,11 @@ async function validate_pub_key(raw_sec_key, b64_pub_key, test_mode) {
     return (!test_mode && pub_key_is_valid)
 }
 
-async function start(b64_key, test_mode) {
+async function start(b64_sec_key, test_mode) {
     const algo = 'ed25519'
     const bitwidth = 2048
     
-    const pbf_sec_key = Buffer.from(b64_key, 'base64')
+    const pbf_sec_key = Buffer.from(b64_sec_key, 'base64')
     const raw_sec_key = await crypto.keys.unmarshalPrivateKey(pbf_sec_key)
     const raw_pub_key = raw_sec_key.public
     const pbf_pub_key = await crypto.keys.marshalPublicKey(raw_pub_key)
@@ -25,7 +25,8 @@ async function start(b64_key, test_mode) {
 
     // Test key integrity:
     if (await validate_pub_key(raw_sec_key, b64_pub_key, test_mode)) {
-        console.log(b64_pub_key)
+        console.log('SECRET KEY (NOT FOR SHARING)\t', b64_sec_key)
+        console.log('PUBLIC KEY (SHARE WITH KUBELT)\t', b64_pub_key)
         process.exit(0)
     } else {
         console.error('Public key cannot validate private key signatures!')
